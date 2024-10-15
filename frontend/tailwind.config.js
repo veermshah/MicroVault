23bd6e1edfdf76db09b683b8638b83978fee414a
jordanjoelson <jjoelson007@gmail.com>
2024-10-15 00:18:17 -0500

    basic set up + font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html', 
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
