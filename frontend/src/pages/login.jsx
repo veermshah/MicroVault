7969558fd0e0652da7890cdcc9a13f6f378ceef9
aaryaravishankar <aaryaravishankar@gmail.com>
2024-10-27 20:11:47 -0500

    login fix

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -5,7 +5,7 @@ import MetaMaskLogin from "../components/MetaMaskLogin";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the App</h1>
+      
       <MetaMaskLogin />
     </div>
   );
